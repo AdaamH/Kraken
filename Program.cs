5a3bd27803a4d6a40771db4e44a18ecb13b72750
Thomas.T <thomastran94@outlook.com>
2019-10-30 14:40:24 +0100

    Addmenu

--- Program.cs
+++ Program.cs
@@ -6,7 +6,38 @@ namespace Kraken
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+bool shouldNotExit = true;
+
+            while (shouldNotExit)
+            {
+
+                Console.WriteLine("1. Add todo");
+                Console.WriteLine("2. Lis todo");
+                Console.WriteLine("3. Exit");
+
+                ConsoleKeyInfo keyPressed = Console.ReadKey(true);
+
+                Console.Clear();
+
+                switch (keyPressed.Key)
+                {
+                    case ConsoleKey.D1:
+                    case ConsoleKey.NumPad1:
+
+                        break;
+
+                    case ConsoleKey.D3:
+                    case ConsoleKey.NumPad3:
+
+                        shouldNotExit = false;
+
+                        break;
+                }
+
+                Console.Clear();
+            }
+                
         }
     }
 }
